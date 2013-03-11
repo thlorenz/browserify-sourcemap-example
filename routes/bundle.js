ec621798fc16fd0450fe8eb5ccc691e345346d51
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-11 08:55:16 -0400

    apdating to changed browserify api (includes source content)

--- routes/bundle.js
+++ routes/bundle.js
@@ -2,7 +2,7 @@
 
 var browserify = require('browserify')
   , path = require('path')
-  , jsroot = path.join(__dirname, '..', 'public', 'js')
+  , jsroot = path.join(__dirname, '..', 'public', 'js');
 
 module.exports = function (app) {
   var bundlePath = '/js/bundle.js';
@@ -11,9 +11,7 @@ module.exports = function (app) {
     browserify()
       .require(path.join(jsroot, 'main.js'), { entry: true })
 
-      // tell browserify that we will make it appear as if the bundle is served from our /public/js folder
-      // that way the browser will request the originals from /js/filename.js which are served by the static file server
-      .bundle({ sourcemapBundle: path.join(jsroot, 'bundle.js') }, function (err, src) {
+      .bundle({ debug: true }, function (err, src) {
         if (err) return console.error(err);
 
         // the bundle doesn't actually exist, we generate it every time it is requested
