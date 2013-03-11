839453a88211e590a1adc71af38b48cf5f4b0fab
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-11 13:20:44 -0400

    removing static server

--- app.js
+++ app.js
@@ -10,8 +10,5 @@ require('./routes/index')(app);
 // generates bundle on the fly 
 require('./routes/bundle')(app);
   
-// we use a static file server to serve the original JavaScript files in order to show them in dev tools
-app.use(express.static(path.join(__dirname, 'public')));
-
 var server = app.listen(3000);
 console.log('Listening on http://localhost:3000');
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@
 module.exports = function (app) {
   app.get('/', function (req, res) {
     // don't need a view engine
-    res.sendfile('views/index.html');
+   res.sendfile('views/index.html');
   });
 };
